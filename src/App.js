ce94ea5a60b033666a7e7ce4ace36dd7a1ffb6cc
ttieman <tiemantanner@gmail.com>
2023-05-18 18:34:22 -0700

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import PortfolioContainer from './components/PortfolioContainer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-const App = () => <PortfolioContainer className="" />
-
-export default App;
